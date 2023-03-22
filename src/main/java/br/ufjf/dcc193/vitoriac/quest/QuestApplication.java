1eb6afbd1f889e98ab2589f859a351e42250ba1f
Vitoria Natalia Caetano <vitorianatalia@ice.ufjf.br>
2023-03-21 21:32:01 -0300

    empty quest project

---
+++ src/main/java/br/ufjf/dcc193/vitoriac/quest/QuestApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.vitoriac.quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/dcc193/vitoriac/quest/QuestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.vitoriac.quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
